2930503202c1e9ffa0ee767653b2b303ff9ee825
Bruce Steedman <bruce.steedman@gmail.com>
2014-02-01 15:16:09 +0000

    Move scraper tests to app_scraper_spec.rb

--- spec/models/planning_app_spec.rb
+++ spec/models/planning_app_spec.rb
@@ -2,39 +2,5 @@ require 'spec_helper'
 
 describe PlanningApp do
 
-  context "::get_new_apps" do
-
-    specify "should populate database with an (inclusive) range of apps" do
-      AppScraper.new.get_new_apps('P', '2014', '0180', '0181')
-      expect(PlanningApp.first.reference).to eq('P/2014/0180')
-      expect(PlanningApp.last.reference).to eq('P/2014/0181')
-    end
-
-    specify "should skip pages for duff application references" do
-      AppScraper.new.get_new_apps('P', '2014', '0181', '0182')
-      expect(PlanningApp.count).to eq(1)
-    end
-
-    specify "should populate correct fields" do
-      field_contents = {reference: 'P/2014/0179',
-                        description: 'Extend play and parking areas.',
-                        applicant: 'Mr A Norman, La Route du Port Elizabeth,'+
-                                    ' St. helier, JE2 3NW',
-                        app_property: 'Mont Nicolle School',
-                        latitude: 49.189235,
-                        longitude: -2.187947}
-      AppScraper.new.get_new_apps('P', '2014', '0179', '0179')
-      field_contents.each do |key, value|
-        expect(PlanningApp.last.send(key)).to eq(value)
-      end
-    end
-
-    specify "should populate correct FK's" do
-      AppScraper.new.get_new_apps('P', '2012', '0181', '0181')
-      expect(PlanningApp.last.app_category.code).to eq('P')
-      expect(PlanningApp.last.app_status.description).to eq('Approved')
-    end
-
-  end # of context
 
 end # of describe
